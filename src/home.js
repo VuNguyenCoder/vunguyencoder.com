728fe7c3f1c268e17b442a22691f9846c00009e8
VuNguyenCoder <anhvu92.it@gmail.com>
2022-04-03 17:00:19 +0000

    Update schedule course

--- src/home.js
+++ src/home.js
@@ -11,10 +11,10 @@ export default function Home()
 		<div style={{padding: '20px'}}>
 			<h4>Lớp học lập trình của Vũ Nguyễn Coder</h4>
 
-			<b>Các khóa học: </b><br/>
+			<b>Các khóa học đang vận hành: </b><br/>
 			<ul>
 				<li>
-					Lập trình cơ bản cùng C++: 
+					Lập trình cơ bản (C/C++) -  
 					Xây dựng căn cơ nền tảng lập trình - {' '}
 					<MoreDetail courseID='cppbasic'/>
 				</li>
@@ -22,10 +22,14 @@ export default function Home()
 					Lập trình hướng đối tượng (C++) - {' '} 
 					<MoreDetail courseID='oopcpp'/>
 				</li>
-				<li>Lập trình nâng cao cùng Java: (Đang xây dựng)</li>
-				<li>Lập trình Web cơ bản cùng HTML/CSS/JS: (Đang xây dựng)</li>
-				<li>Lập trình Game cơ bản cùng C++/OpenGL: (Đang xây dựng)</li>
-				<li>Quản trị hệ thống Linux: (Đang xây dựng)</li>
+			</ul>
+			<b>Các khóa học sắp mở cửa </b><br/>
+			<ul>
+				<li>Lập trình cơ bản (Python) - Mở lớp 1/5/2022</li>
+				<li>Lập trình hướng đối tượng (Java) - Mở lớp 1/6/2022</li>
+				<li>Lập trình GUI (C++/Qt) - Mở lớp 1/7/2022</li>
+				<li>Lập trình game nhập môn (C++/OpenGL, Unity) - Mở lớp 1/7/2022</li>
+				<li>Quản trị Linux - Mở lớp 1/8/2022</li>
 			</ul>
 			<p>Hot line liên hệ: <b>088 995 8883</b> (Nếu máy bận xin để lại tin nhắn)</p>
 		</div>
